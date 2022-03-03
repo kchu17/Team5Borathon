82f1ea85cd359d4993865f8f4949a902310ac660
Daniel Vazquez Guevara <daniel@dvazquezgue-a01.vmware.com>
2022-03-03 15:26:25 -0800

    adding getCustomer operation

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello():
     return "<h1 style='color:blue'>Hello There! This file was edited locally.</h1>"
 
+@app.route("/GetCustomerAccountByAccountNumber")
+def getCustomerAccountByAccountNumber():
+    return "<h1 style='color:blue'>Getting Customer Account By Account Number.</h1>"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
 
