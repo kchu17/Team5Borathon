519d4e3932aaac505685d60b6a87413529f4ebfe
Daniel Vazquez Guevara <daniel@dvazquezgue-a01.vmware.com>
2022-03-03 15:44:32 -0800

    returning JSON attempt 2

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,7 +10,7 @@ def hello():
 @app.route("/GetCustomerAccountByAccountNumber")
 def getCustomerAccountByAccountNumber():
     my_dict = {"firstName": 'Melissa', "lastName": 'Testing'}
-    return my_dict
+    return jsonify(my_dict)
     #return "<h1 style='color:blue'>Getting Customer Account By Account Number.</h1>"
 
 if __name__ == "__main__":
