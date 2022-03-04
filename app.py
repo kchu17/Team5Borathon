fb8a3c6ab0b895aeec263a28fa0840696cf59ab1
Daniel Vazquez Guevara <daniel@dvazquezgue-a01.vmware.com>
2022-03-04 00:34:01 -0800

    Flask connection to AWS RDB service sucessful

--- app.py
+++ app.py
@@ -1,17 +1,59 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, request, jsonify
+import pymysql
 
 app = Flask(__name__)
 
+conn = pymysql.connect(
+        host= 'team5db-1.cjh7bpk5je23.us-east-1.rds.amazonaws.com', 
+        port = 3306,
+        user = 'admin', 
+        password = 'Random!PassT5#',
+        db = 'bankDB',
+        )
+
+# create DB
+# with conn.cursor() as cur:
+#     cur.execute('create database bankDB;')
+
+# create table
+# cursor=conn.cursor()
+# create_table="""
+# create table testTable1 (name varchar(200),email varchar(200),comment varchar(200),gender varchar(20))
+# """
+# cursor.execute(create_table)
+
+def insert_details(name,email,comment,gender):
+    cur = conn.cursor()
+    cur.execute("INSERT INTO testTable1 (name,email,comment,gender) VALUES (%s,%s,%s,%s)", (name,email,comment,gender))
+    conn.commit()
+#insert_details("John", "john@mail.com","Testing from DB", "Male")   
+
+def get_details():
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM testTable1")
+    details = cur.fetchall()
+    return details
+
+# def select(name):
+#     cur=conn.cursor()
+#     cur.execute("SELECT comment FROM testTable1 WHERE name = %s", (name,email,comment,gender))
+#     conn.commit()
+
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There! This file was edited locally.</h1>"
+    return "<h1 style='color:blue'>Hello There!</h1>"
 
-@app.route("/GetCustomerAccountByAccountNumber")
+@app.route("/GetCustomerAccountByAccountNumber", methods=['GET'])
 def getCustomerAccountByAccountNumber():
-    my_dict = {"firstName": 'Belissa', "lastName": 'Testing'}
+    accountNumber = request.args.get('accountNumber')
+    my_dict = {'firstName': 'Belissa', 'account': accountNumber}
     return jsonify(my_dict)
-    #return "<h1 style='color:blue'>Getting Customer Account By Account Number.</h1>"
+
+@app.route("/GetJohnTest", methods=['GET'])
+def getJohnTest():
+    result = get_details()
+    my_dict = {'firstName': 'Belissa', 'account': result}
+    return jsonify(my_dict)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
