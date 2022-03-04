f3310cd1a1a8aab0cb475542e48d4f479bf975a2
Daniel Vazquez Guevara <daniel@dvazquezgue-a01.vmware.com>
2022-03-04 08:18:11 -0800

    Creating DBs in AWS RDB

--- create_tables.py
+++ create_tables.py
@@ -8,14 +8,44 @@ conn = pymysql.connect(
         db = 'bankDB',
         )
 
+# CREATE TABLE Account (
+#     accountID INT NOT NULL,
+#     acountNumber VARCHAR(200) NOT NULL,
+#     balance FLOAT NOT NULL,
+#     status VARCHAR(200) NOT NULL,
+#     PRIMARY KEY (accountID),
+# );
 
-# create table
-with conn.cursor() as cur:
-    cur.execute('create table testTable1 (name varchar(200),email varchar(200),comment varchar(200),gender varchar(20));')
+# CREATE TABLE Customer (
+#     customerID INT NOT NULL,
+#     firstName VARCHAR(200) NOT NULL,
+#     lastName VARCHAR(200) NOT NULL,
+#     PRIMARY KEY (customerID),
+#     accountID INT NOT NULL,
+#     FOREIGN KEY (accountID) REFERENCES Account(accountID)
+# );
 
-# alternative    
-# cursor=conn.cursor()
-# create_table="""
-# create table testTable1 (name varchar(200),email varchar(200),comment varchar(200),gender varchar(20))
-# """
-# cursor.execute(create_table)
+# CREATE TABLE Transaction (
+#     transactionID INT NOT NULL,
+#     amount FLOAT NOT NULL,
+#     transactionType VARCHAR(200) NOT NULL,
+#     PRIMARY KEY (transactionID),
+#     accountID INT NOT NULL,
+#     FOREIGN KEY (accountID) REFERENCES Account(accountID)
+# );
+
+#- ---------- Actual code ----------
+# create Account table
+# with conn.cursor() as cur:
+#     cur.execute('CREATE TABLE Account (accountID INT NOT NULL, acountNumber VARCHAR(200) NOT NULL, balance FLOAT NOT NULL, status VARCHAR(200) NOT NULL, PRIMARY KEY (accountID));')
+
+
+# create Customer table
+# with conn.cursor() as cur:
+#     cur.execute('CREATE TABLE Customer (customerID INT NOT NULL, firstName VARCHAR(200) NOT NULL, lastName VARCHAR(200) NOT NULL, PRIMARY KEY (customerID), accountID INT NOT NULL, FOREIGN KEY (accountID) REFERENCES Account(accountID));')
+
+# create Transaction table
+# with conn.cursor() as cur:
+#     cur.execute('CREATE TABLE Transaction (transactionID INT NOT NULL, amount FLOAT NOT NULL, transactionType VARCHAR(200) NOT NULL, PRIMARY KEY (transactionID), accountID INT NOT NULL, FOREIGN KEY (accountID) REFERENCES Account(accountID));')
+
+ 
